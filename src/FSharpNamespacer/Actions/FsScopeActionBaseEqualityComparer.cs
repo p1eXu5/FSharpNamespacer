5b7030cb7c174d9277ff1faf705212d25a6a03f8
p1eXu5 <v1adp1exu5@gmail.com>
2025-09-08 08:10:26 +0300

    chore: add FsScopeActionBaseEqualityComparer

---
+++ src/FSharpNamespacer/Actions/FsScopeActionBaseEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FSharpNamespacer.Actions
+{
+    internal sealed class FsScopeActionBaseEqualityComparer : IEqualityComparer<FsScopeActionBase>
+    {
+        private static FsScopeActionBaseEqualityComparer _default;
+
+        public static FsScopeActionBaseEqualityComparer Default
+        {
+            get
+            {
+                if (_default is null)
+                {
+                    _default = new FsScopeActionBaseEqualityComparer();
+                }
+
+                return _default;
+            }
+        }
+
+        public bool Equals(FsScopeActionBase x, FsScopeActionBase y)
+            => x.DisplayText.Equals(y.DisplayText, StringComparison.Ordinal);
+
+        public int GetHashCode(FsScopeActionBase obj) => obj.GetHashCode();
+    }
+}
